2b3c8dbf55828dbc3187f642b953ba3a970172bd
JoseAugustoDev <75041668+JoseAugustoDev@users.noreply.github.com>
2022-09-10 22:50:15 -0300

    Reset

--- js/contact.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".mobile-nav-bar");
-const button = document.querySelector(".menu")
-
-button.addEventListener('click', () => {
-    hamburger.style.display = "flex";
-})
