cd75eaabcddf48fca72aa8fba9e649758789c9f7
JoseAugustoDev <75041668+JoseAugustoDev@users.noreply.github.com>
2022-09-10 23:11:38 -0300

    New site using React

---
+++ joseaugustodev/src/components/header.js
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <div className="header">
+            <a href="/"><h1>Jose Augusto <span>Dev</span></h1></a>
+            <nav>
+                <ul>
+                    <a href="/"><li>Home</li></a>
+                    <a href="/"><li>Projects</li></a>
+                    <a href="/"><li>About</li></a>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
